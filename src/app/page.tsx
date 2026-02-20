14528d76ca067bf753691592c2249f7bf963c1f6
wdc35333 <wdc35333@gmail.com>
2026-02-20 22:35:25 +0900

    [0.1.5] 메인 페이지에 data.json 파일에서 불러온 데이터를 기반으로 Box 컴포넌트 출력

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,42 @@
 'use client';
 
-import { useData } from '@/hooks/useData';
+import Box, { BoxItem } from '@/components/box';
+import useData from '@/hooks/useData';
+
+const CANVAS_WIDTH = 1024;
+const CANVAS_HEIGHT = 768;
 
 export default function DashBoard() {
-  const { data, error, isLoading } = useData();
+  const { data, error, isLoading } = useData<BoxItem[]>();
+  const boxList = data ?? [];
+
+  const layoutBounds = boxList.reduce(
+    (acc, item) => {
+      const right = item.shape.left + item.shape.width;
+      const bottom = item.shape.top + item.shape.height;
+
+      return {
+        maxRight: Math.max(acc.maxRight, right),
+        maxBottom: Math.max(acc.maxBottom, bottom),
+      };
+    },
+    { maxRight: 1, maxBottom: 1 },
+  );
+
+  const scaleX = CANVAS_WIDTH / layoutBounds.maxRight;
+  const scaleY = CANVAS_HEIGHT / layoutBounds.maxBottom;
 
   return (
-    <main className="max-w-[1024px] max-h-[768px] overflow-y-auto">
+    <main className="w-[1024px] h-[768px] overflow-hidden border">
       {error && <p>{error}</p>}
       {!error && isLoading && <p>불러오는 중...</p>}
-      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+      {!error && !isLoading && (
+        <section className="relative w-full h-full">
+          {boxList.map((item) => (
+            <Box key={item.box_id} item={item} scaleX={scaleX} scaleY={scaleY} />
+          ))}
+        </section>
+      )}
     </main>
   );
 }
